8bd30ffc9ef69a80fea834f83f1bab780449bea8
Kazuhiro Kawachi <kawachi@example.com>
2019-09-23 23:48:13 +0900

    rest server

--- blog_server/settings.py
+++ blog_server/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'rest_framework',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 2,
+    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
+}
--- blog_server/urls.py
+++ blog_server/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include
+
+from blog.urls import router as blog_router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(blog_router.urls)),
 ]
